0ba027bae5ab5053da91b062d5875ecc6c1d2d2c
Mirela Rafaela Machado <mirela.rafaela@hotmail.com>
2020-09-03 20:47:27 -0300

    armazenamento offline
    registro de usuario no bd e usuarios service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { IonicStorageModule } from '@ionic/storage';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+     IonicModule.forRoot(),
+      AppRoutingModule,
+      IonicStorageModule.forRoot({
+        name: '__mydb',
+        driverOrder: ['indexeddb', 'sqlite', 'websql']
+      })
+    ],
   providers: [
     StatusBar,
     SplashScreen,
